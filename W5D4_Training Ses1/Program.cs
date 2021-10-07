6e660f3457d6c633a6a48796ee195aee65d64ed3
Akhilesh235 <90238925+Akhilesh235@users.noreply.github.com>
2021-10-07 10:38:36 +0800

    To Upper and ToLower

--- W5D4_Training Ses1/Program.cs
+++ W5D4_Training Ses1/Program.cs
@@ -10,44 +10,73 @@ namespace W5D4_Training_Ses1
     {
         static void Main(string[] args)
         {
-            String_BCL_Examples();
+            Console.WriteLine("enter any string");
+            var usr_input = Console.ReadLine();
+            Console.WriteLine("string in lower case " + usr_input.ToLower());
+            Console.WriteLine("string in upper case " + usr_input.ToUpper());
+
+            Console.ReadLine();
+
+            //String_BCL_Examples();
         }
 
         static void String_BCL_Examples()
         {
+            List<String> lst = new List<string>();
+            
+            
             //compare
             string str1 = "SampleDemo";
-            string str2 = "SampleDemo";
+            string str2 = "sampleDemo";
 
-            int res = str1.CompareTo(str2);
-            Console.WriteLine("result of string compare is:  " + res );
-            //if same, the result will be 0 
+            lst.Add(str1);
+            lst.Add(str1);
+            
+            foreach(string temp in lst)
+            {
+                Console.WriteLine(temp);
+            }
+
+            var arr = lst.ToArray();
+            Array.Sort(arr);
+            Console.WriteLine("\nAfter sorting");
+            foreach (string temp in arr)
+            {
+                Console.WriteLine(temp);
+            }
+
+
+            Console.Read();
+
+            //int res = str1.CompareTo(str2);
+            //Console.WriteLine("result of string compare is:  " + res );
+            ////if same, the result will be 0 
             
 
-            string str3 = "a";
-            string str4 = "b";
+            //string str3 = "a";
+            //string str4 = "b";
 
-            int res1 = str3.CompareTo(str4);
-            Console.WriteLine("result of string compare is:  " + res1);
-            //if different the result will be -1 // based on ASCII value
+            //int res1 = str3.CompareTo(str4);
+            //Console.WriteLine("result of string compare is:  " + res1);
+            ////if different the result will be -1 // based on ASCII value
            
 
-            string str5 = "b";
-            string str6 = "a";
+            //string str5 = "b";
+            //string str6 = "a";
 
-            int res3 = str5.CompareTo(str6);
-            Console.WriteLine("result of string compare is:  " + res3);
-            //if different the result will be 1 // based on ASCII value // b has a great ASCII value than a
+            //int res3 = str5.CompareTo(str6);
+            //Console.WriteLine("result of string compare is:  " + res3);
+            ////if different the result will be 1 // based on ASCII value // b has a great ASCII value than a
           
 
 
-            string str7 = "SampleDemo";
-            string str8 = "sampleDemo";
+            //string str7 = "SampleDemo";
+            //string str8 = "sampleDemo";
 
-            int res4 = str7.CompareTo(str8);
-            Console.WriteLine("result of string compare is:  " + res4);
-            //if same, the result will be 1
-            Console.ReadLine();
+            //int res4 = str7.CompareTo(str8);
+            //Console.WriteLine("result of string compare is:  " + res4);
+            ////if same, the result will be 1
+            //Console.ReadLine();
         }
     }
 }
