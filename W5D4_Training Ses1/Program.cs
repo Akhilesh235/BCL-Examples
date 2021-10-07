251d4ffb551168e598c85468755325c78be68350
Akhilesh235 <90238925+Akhilesh235@users.noreply.github.com>
2021-10-07 10:25:11 +0800

    string compare

--- W5D4_Training Ses1/Program.cs
+++ W5D4_Training Ses1/Program.cs
@@ -22,7 +22,7 @@ namespace W5D4_Training_Ses1
             int res = str1.CompareTo(str2);
             Console.WriteLine("result of string compare is:  " + res );
             //if same, the result will be 0 
-            Console.ReadLine();
+            
 
             string str3 = "a";
             string str4 = "b";
@@ -30,6 +30,23 @@ namespace W5D4_Training_Ses1
             int res1 = str3.CompareTo(str4);
             Console.WriteLine("result of string compare is:  " + res1);
             //if different the result will be -1 // based on ASCII value
+           
+
+            string str5 = "b";
+            string str6 = "a";
+
+            int res3 = str5.CompareTo(str6);
+            Console.WriteLine("result of string compare is:  " + res3);
+            //if different the result will be 1 // based on ASCII value // b has a great ASCII value than a
+          
+
+
+            string str7 = "SampleDemo";
+            string str8 = "sampleDemo";
+
+            int res4 = str7.CompareTo(str8);
+            Console.WriteLine("result of string compare is:  " + res4);
+            //if same, the result will be 1
             Console.ReadLine();
         }
     }
